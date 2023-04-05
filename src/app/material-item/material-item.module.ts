42797ab52672211e9dbe4f46cf701e91429c7512
your_username <patrizio.sbrozzi@gmail.com>
2023-04-05 10:15:48 +0200

    buttons comp ok - todo date picker

--- src/app/material-item/material-item.module.ts
+++ src/app/material-item/material-item.module.ts
@@ -4,6 +4,10 @@ import { CommonModule } from '@angular/common';
 import { MaterialItemRoutingModule } from './material-item-routing.module';
 import { ButtonsComponent } from './buttons/buttons.component';
 
+import {MatRadioModule} from '@angular/material/radio';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -11,6 +15,10 @@ import { ButtonsComponent } from './buttons/buttons.component';
   ],
   imports: [
     CommonModule,
+    MatRadioModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
     MaterialItemRoutingModule
   ]
 })
